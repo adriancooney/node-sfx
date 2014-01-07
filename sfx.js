bb16c0c09564750717ecffbd07eb8fdc21bd450c
Adrian Cooney <cooney.adrian@gmail.com>
2014-01-07 21:04:42 +0000

    Fixing sexist error message

--- sfx.js
+++ sfx.js
@@ -96,7 +96,7 @@ var sfx = {
 
         if(process.platform !== "darwin") {
             if(callback) return callback();
-            else return console.log("`say` is a darwin only feature. Sorry dudes.");
+            else return console.log("`say` is a darwin only feature. Sorry, for now.");
         }
 
         var voices = "Agnes,Albert,Alex,Bad News,Bahh,Bells,Boing,Bruce,Bubbles,Cellos,Deranged,Fred,Good News,Hysterical,Junior,Kathy,Pipe Organ,Princess,Ralph,Trinoids,Vicki,Victoria,Whisper,Zarvox".split(",").map(function(v) {
