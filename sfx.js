c91a908c2017425037cf3360316fadc69e93020a
Adrian Cooney <cooney.adrian@gmail.com>
2014-01-07 18:23:38 +0000

    Initial working commit

---
+++ sfx.js
@@ -0,0 +1,163 @@
+var exec = require("child_process").exec,
+	fs = require("fs");
+
+const DEBUG = true;
+const LIBRARY = __dirname + "/sounds/";
+
+var sfx = {
+	library: {},
+	config: {},
+
+	/**
+	 * Play a sound.
+	 * @param  {String} sound  The sound file or name of sound in library
+	 * @param  {Number} volume Volume between 0-100
+	 * @param  {Number} rate   Rate at which the sound should play
+	 */
+	play: function(sound, volume, rate) {
+		var config = sfx.config,
+			command = config.play.command;
+
+		// Play a random sound
+		if(sound == "random") sound = Object.keys(sfx.library)[Math.floor(Math.random() * Object.keys(sfx.library).length)];
+
+		// Volume
+		if(volume && config.play.options.volume) {
+			var v = config.play.options.volume;
+			command += " " + sfx.format(v.syntax, { 
+				// Denormalize via the range
+				volume: (v.range[1] - v.range[0]) * (volume/100)
+			});
+		}
+
+		// Rate
+		if(rate && config.play.options.rate) command += " " + sfx.format(config.play.options.rate.syntax, { rate: rate });
+
+		// Play the sound
+		sfx.getSound(sound, function(err, path) {
+			command += " " + path;
+
+			sfx.run(command);
+		});
+	},
+
+	/**
+	 * Quick helper function to format a string of variables
+	 * @param  {String} string String with variables in the form of ":varName"
+	 * @param  {Object} values Object containing variable values
+	 * @return {String}        Formatted string
+	 */
+	format: function(string, values) {
+		return string.replace(/\:(\w+)/g, function(match, value) {
+			return values[value];
+		});
+	},
+
+	/**
+	 * Retrieve and test a sound via a path or name.
+	 * Works kindof like `require`:
+	 * 	1. Check if sound is at path
+	 * 	2. Check if sound is in ./sound
+	 * 	
+	 * @param  {String}   path 	   Path or name of sound
+	 * @param  {Function} callback Callback with (err, soundPath)
+	 */
+	getSound: function(path, callback) {
+		fs.exists(path, function(exists) {
+			// Sound exists at given path
+			if(exists) callback(null, path);
+			else {
+				// Check if the sound is in the library
+				if(sfx.library[path]) callback(null, LIBRARY + sfx.library[path]);
+				else callback(new Error("Sound not found."));
+			}
+		});
+	},
+
+	/**
+	 * Say a message. Mac/Darwin only.
+	 * @param  {String} message Message to say aloud
+	 * @param  {String} voice   Voice. Accepts "random" and will play with a random voice
+	 */
+	say: function(message, voice) {
+		if(process.platform !== "darwin") return console.log("`say` is a darwin only feature. Sorry dudes.");
+
+		var voices = "Agnes,Albert,Alex,Bad News,Bahh,Bells,Boing,Bruce,Bubbles,Cellos,Deranged,Fred,Good News,Hysterical,Junior,Kathy,Pipe Organ,Princess,Ralph,Trinoids,Vicki,Victoria,Whisper,Zarvox".split(",").map(function(v) { return v.toLowerCase(); });
+
+		if(voice == "random") voice = voices[Math.floor(Math.random() * voice.length)];
+		else if(voice && voices.indexOf(voice) == -1) throw new Error("Voice does not exist!");
+
+		sfx.run("say " + (voice ? "-v " + voice + " " : "") + message);
+	},
+
+	/**
+	 * Run a command.
+	 * @param  {String} command Command string
+	 * @return {[type]}         [description]
+	 */
+	run: function(command) {
+		DEBUG && console.log("Running command: ", command);
+		exec(command);
+	},
+
+	/**
+	 * Verify an inputted configuration.
+	 * @param  {Object} config 
+	 * @return {Object}        The configuation object
+	 */
+	verifyConfig: function(config) {
+		Object.keys(config).forEach(function(command) {
+			var c = config[command];
+
+			if(!c.command) throw new Error("sfx: Bad configuration. Please specify a command attribute in the `" + command + "`");
+			if(!c.options) return c.options = {};
+
+			// Loop over the options
+			Object.keys(c.options).forEach(function(option) {
+				var o = c.options[option];
+				if(!o.syntax) throw new Error("sfx: Bad configuration. Please specify the syntax attribute in the option `" + option + "` in the `" + command + "` command");
+			});
+		});
+
+		return config;
+	},
+
+	/**
+	 * Initilize sfx
+	 * @return {sfx}
+	 */
+	init: function() {
+		// Verify the configuration
+		try {
+			sfx.config = sfx.verifyConfig(require("./platform/" + process.platform + ".json"));
+		} catch(err) {
+			if(err.code == "ENOENT") console.log("sfx: Platform not supported yet.");
+			else {
+				throw err;
+			}
+		}
+
+		// Populate the library
+		// Syncronous to prevent unpopulated library from being used.
+		var files = fs.readdirSync(LIBRARY);
+
+		files.forEach(function(file) {
+			var split = file.split(".");
+			split.pop(); // Pop off the extension
+
+			var name = split.join(".").toLowerCase();
+			sfx.library[name] = file;
+
+			// Bind the to sfx
+			sfx[name] = sfx.play.bind(sfx, name);
+		});
+
+		// Random
+		sfx.random = sfx.play.bind(sfx, "random");
+
+
+		return sfx;
+	}
+};
+
+module.exports = sfx.init();
---
+++ test/sfx.js
@@ -0,0 +1,91 @@
+var sfx = require("./../sfx"),
+	assert = require("assert"),
+	path = require("path");
+
+/**
+ * Unit testing this is hard and annoying.
+ *
+ * I can see it becoming an absolute cacophony of sound.
+ */
+describe("sfx", function() {
+	describe("say", function() {
+		it("should say 'something'", function() {
+			sfx.say("something");
+		});
+
+		it("should say 'something' with voice bubbles", function() {
+			sfx.say("something", "bubbles");
+		});
+
+		it("should say 'something' with a random voice", function() {
+			sfx.say("something", "random");
+		});
+	});
+
+	describe("play", function() {
+		it("should play the sound ping", function() {
+			sfx.play("ping");
+		});
+
+		it("should play the sound ping with volume", function() {
+			sfx.play("ping", 10);
+		});
+
+		it("should play the sound ping with volume and rate", function() {
+			sfx.play("ping", 10, 1);
+		});
+
+		it("should play a sound exposed on the sfx object", function() {
+			sfx.ping();
+		});
+
+		it("should play a sound exposed on the sfx object with volume", function() {
+			sfx.ping(100);
+		});
+
+		it("should play a sound exposed on the sfx object with volume and rate", function() {
+			sfx.ping(10, 100);
+		});
+
+		it("should play a random sound", function() {
+			sfx.play("random");
+		});
+
+		it("should play a random sound via the sfx.random function", function() {
+			sfx.random();
+		});
+
+		it("should play a sound at a path", function() {
+
+		});
+	});
+
+	describe("format", function() {
+		it("should format a string", function() {
+			assert(sfx.format(":volume", { volume: "foo"}) == "foo");
+		});
+	});
+
+	describe("getSound", function() {
+		it("should test a path to see if a sound exists at it and fail gracefully", function(next) {
+			sfx.getSound("/path/lol", function(err, path) {
+				assert(err !== null);
+				next();
+			});	
+		});
+
+		it("should test a sound at a real path", function(next) {
+			sfx.getSound(path.resolve(__dirname, "../sounds/Ping.aiff"), function(err, path) {
+				assert(path && err == null);
+				next();
+			});
+		});
+
+		it("should test a sound with a name", function(next) {
+			sfx.getSound("ping", function(err, path) {
+				assert(path && path !== "ping" && err == null);
+				next();
+			});
+		});
+	});
+});
